323aa1f97cbb6ec22f425b9376ea9376825261b7
Magdi Hajjaj <159680859+MagdiHajjaj@users.noreply.github.com>
2025-11-30 16:16:11 -0500

    Fix build script to use correct source file path

--- PartA/build.sh
+++ PartA/build.sh
@@ -1,7 +1,7 @@
 if [ ! -d "bin" ]; then
     mkdir bin
 else
-	rm bin/*
+	rm -f bin/*
 fi
 
-g++ -std=c++17 -g -O0 -I . -o bin/TaMarkingPartA PartA/TaMarking_101229043_101299841.cpp
+g++ -std=c++17 -g -O0 -I . -o bin/TaMarkingPartA TaMarking_101229043_101299841.cpp
